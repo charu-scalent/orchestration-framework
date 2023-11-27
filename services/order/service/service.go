4bad53adccaede9c4792d343bea6ec7359e557f3
swati-scalent <swati@scalent.io>
2023-11-27 21:17:37 +0530

    order service structure added

---
+++ services/order/service/service.go
@@ -0,0 +1,11 @@
+package service
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/scalent-io/orchestration-framework/entity"
+	"github.com/scalent-io/orchestration-framework/pkg/errors"
+)
+
+type OrderService interface {
+	Load(ctx *gin.Context, userID, orderID string) (*entity.Order, errors.Response)
+}
